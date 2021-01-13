35aa1ad2bc917305b98aedd75fb6c6a1f1afa6eb
Hemasundara Rao Pottangi <hemapottangi@gmail.com>
2021-01-13 08:50:13 +0000

    Formatting Change

--- src/main/java/org/springframework/samples/petclinic/WebSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/WebSecurityConfig.java
@@ -13,31 +13,20 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
 	}
+
 }
